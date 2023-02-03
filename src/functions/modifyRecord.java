2506e8e069618f9ffbc25d33dbc36cbe3cbccb95
Kiran <kkp792001@gmail.com>
2023-02-03 17:15:41 +0530

    fixing issue in modify record

--- src/functions/modifyRecord.java
+++ src/functions/modifyRecord.java
@@ -1,7 +1,7 @@
 package functions;
 import java.sql.*;
 
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.GridLayout;
 import java.awt.TextField;
 import java.sql.Connection;
@@ -16,21 +16,23 @@ import javax.swing.JPanel;
 public class modifyRecord extends JFrame {
 	Student st = new Student();
 	JButton rn3,titl, back;
-	JLabel rn1,rn, name, p, m, c, e, com, ed;
+	JLabel rn1,rn, name, p, m, c, e1, com, ed;
 	TextField rn2,trn, tname, tp, tm, tc, te, tcom;
-	JPanel p1, p2, p3,p4;
+	JPanel p1, p2, p3,p4,mp;
 	String rollno;
 	String nam;
 	String ph, ma, ch, en, comp;
 	Connection con;
 	PreparedStatement ps;
 	int rn4;
-	public modifyRecord(){
-		
+
+	public modifyRecord() {
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
-			ps = con.prepareStatement("update student set Name=?,Physics=?,Chemistry=?,maths=?,english=?,computer=? where rollno=?");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+			ps = con.prepareStatement(
+					"update student set Name=?,Physics=?,Chemistry=?,maths=?,english=?,computer=? where rollno=?");
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -38,28 +40,30 @@ public class modifyRecord extends JFrame {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		FlowLayout f = new FlowLayout();
 		setLayout(f);
 		setVisible(true);
 		setSize(1080, 540);
-		f.setVgap(150);
+//		f.setVgap(150);
+		
+		GridLayout gl=new GridLayout(4,1);
+		gl.setVgap(40);
 
+		mp = new JPanel(gl);
 		p1 = new JPanel(new GridLayout(1, 2));
 		p2 = new JPanel(new GridLayout(7, 2));
 		p3 = new JPanel(new GridLayout(1, 1));
-		p4 = new JPanel(new GridLayout(1,3));
-		
+		p4 = new JPanel(new GridLayout(1, 3));
+
 		rn1 = new JLabel("Enter RollNo: ");
 		rn2 = new TextField();
 		rn3 = new JButton("Update entered record");
 		p4.add(rn1);
 		p4.add(rn2);
 		p4.add(rn3);
-		
-		add(p4);		
+		mp.add(p4);
 
-		titl = new JButton("Modify Student Record");
 		rn = new JLabel("Enter Roll No ");
 		trn = new TextField();
 		name = new JLabel("Enter Name ");
@@ -70,15 +74,17 @@ public class modifyRecord extends JFrame {
 		tm = new TextField();
 		c = new JLabel("Enter Chemistry Marks ");
 		tc = new TextField();
-		e = new JLabel("Enter English Marks ");
+		e1 = new JLabel("Enter English Marks ");
 		te = new TextField();
 		com = new JLabel("Enter Computer Marks ");
 		tcom = new TextField();
 		ed = new JLabel();
+		titl = new JButton("Modify Student Record");
+		titl.setBounds(0, 0, 50, 50);
 		back = new JButton("Back");
-		
+
 		rn3.addActionListener(e -> {
-			rn4=Integer.valueOf(rn2.getText());
+			rn4 = Integer.valueOf(rn2.getText());
 			rollno = String.valueOf(rn4);
 			trn.setText(rollno);
 			trn.setEditable(false);
@@ -93,31 +99,30 @@ public class modifyRecord extends JFrame {
 				e1.printStackTrace();
 			}
 			p4.setVisible(false);
+
+			p2.add(rn);
+			p2.add(trn);
+			p2.add(name);
+			p2.add(tname);
+			p2.add(p);
+			p2.add(tp);
+			p2.add(m);
+			p2.add(tm);
+			p2.add(c);
+			p2.add(tc);
+			p2.add(e1);
+			p2.add(te);
+			p2.add(com);
+			p2.add(tcom);
+			p1.add(titl);
+			p1.add(back);
+			mp.add(p2);
+			mp.add(p1);
+			mp.add(p3);
+
 		});
 
-		p2.add(rn);
-		p2.add(trn);
-		p2.add(name);
-		p2.add(tname);
-		p2.add(p);
-		p2.add(tp);
-		p2.add(m);
-		p2.add(tm);
-		p2.add(c);
-		p2.add(tc);
-		p2.add(e);
-		p2.add(te);
-		p2.add(com);
-		p2.add(tcom);
-		p1.add(titl);
-		p1.add(back);
-		p3.add(ed);
-		add(p2);
-		add(p1);
-		add(p3);
-
 		titl.addActionListener(e -> {
-			ed.setText(" Student Record Updated");
 			nam = tname.getText();
 			ph = tp.getText();
 			ma = tm.getText();
@@ -150,5 +155,6 @@ public class modifyRecord extends JFrame {
 			entry_editMenu eem = new entry_editMenu();
 			setVisible(false);
 		});
+		add(mp);
 	}
 }
