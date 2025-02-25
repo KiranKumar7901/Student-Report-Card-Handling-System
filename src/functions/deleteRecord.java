0b4fafa253ae683a85113a9665fe983070af317e
Kiran Kumar <87652873+KiranKumar7901@users.noreply.github.com>
2025-02-25 10:59:16 +0530

    Update deleteRecord.java

--- src/functions/deleteRecord.java
+++ src/functions/deleteRecord.java
@@ -19,7 +19,8 @@ public class deleteRecord extends JFrame {
 	public deleteRecord(){
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+			// con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+			con=DriverManager.getConnection("b5ybjb0rguk9bnfnm3qr-mysql.services.clever-cloud.com","uwfsctyutchk44qa","uwfsctyutchk44qa");
 			ps = con.prepareStatement("delete from student where rollno=?");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
