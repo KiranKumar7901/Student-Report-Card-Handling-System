4d3e6d7b83b5081cba4320b4760b646dd896fb46
Kiran Kumar <87652873+KiranKumar7901@users.noreply.github.com>
2025-02-25 11:13:58 +0530

    Update StudentReportCard.java

--- src/functions/StudentReportCard.java
+++ src/functions/StudentReportCard.java
@@ -29,7 +29,7 @@ public class StudentReportCard extends JFrame {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			// con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-			con=DriverManager.getConnection("b5ybjb0rguk9bnfnm3qr-mysql.services.clever-cloud.com","uwfsctyutchk44qa","uwfsctyutchk44qa");
+			con=DriverManager.getConnection("mysql://uwfsctyutchk44qa:HhXHnP4xtx4DSbaCgIsN@b5ybjb0rguk9bnfnm3qr-mysql.services.clever-cloud.com:3306/b5ybjb0rguk9bnfnm3qr","uwfsctyutchk44qa","uwfsctyutchk44qa");
 			ps = con.prepareStatement("Select * from Student where rollno=?");
 		} catch (ClassNotFoundException e2) {
 			e2.printStackTrace();
