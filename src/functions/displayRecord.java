4ebde3a8866c8016fffbc0aeff017c286f2b322f
Kiran Kumar <87652873+KiranKumar7901@users.noreply.github.com>
2025-02-25 17:17:57 +0530

    Update displayRecord.java

--- src/functions/displayRecord.java
+++ src/functions/displayRecord.java
@@ -17,8 +17,7 @@ public class displayRecord extends JFrame {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			// con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-			con=DriverManager.getConnection("mysql://uwfsctyutchk44qa:HhXHnP4xtx4DSbaCgIsN@b5ybjb0rguk9bnfnm3qr-mysql.services.clever-cloud.com:3306/b5ybjb0rguk9bnfnm3qr","uwfsctyutchk44qa","uwfsctyutchk44qa");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
 			stmt =con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 			rs = stmt.executeQuery("Select * from Student");
 		} catch (ClassNotFoundException e2) {
